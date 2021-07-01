9bb56b995bea4782a1af9396b6abc48cf3ca6506
Yaswanth JK <39457010+Yaswanth3277@users.noreply.github.com>
2021-07-01 14:29:14 -0500

    test update

--- application.py
+++ application.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, url_for
+import os
 
 
 application = Flask(__name__)
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+word_file = os.path.join(basedir, 'static/AliceInWonderland.txt')
+
 @application.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    lines = []
+    with open(word_file, 'rb') as fileinput:
+        for line in fileinput:
+            for words in line.split():
+                if str(words)[2:-1] == "Adventures":
+                    lines.append(line.decode())
+    return render_template('index.html', lines = lines)
 
 
 if __name__ == '__main__':
